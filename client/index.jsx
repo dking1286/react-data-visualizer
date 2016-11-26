f232e86810c4a8d10f4b1b3f5828651f2f367bb7
Daniel King <daniel.oliver.king@gmail.com>
2016-11-25 20:46:54 -0800

    Lint index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const App = () =>
-  <h1>Hello world!</h1>;
+const App = () => (
+  <h1>Hello world!</h1>
+);
 
 render(
   <App />,

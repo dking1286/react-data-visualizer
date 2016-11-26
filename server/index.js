b43b1e2d2c7efd769e3ed2192466ff41396ce8e0
Daniel King <daniel.oliver.king@gmail.com>
2016-11-25 20:32:12 -0800

    Server serves json data also

--- server/index.js
+++ server/index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+const ads = require('./ads');
+const adsMetrics = require('./ads_metrics');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 app.use(express.static(path.join(__dirname, '../client')));
+
+
+app.get('/ads', (req, res) => {
+  res.json(ads);
+});
+
+app.get('/ads_metrics', (req, res) => {
+  res.json(adsMetrics);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
